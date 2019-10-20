6427e0f1d03cf1929cba2357ec5d24076e518c7a
OrtizJosh <joshwortiz@gmail.com>
2019-10-20 11:20:32 -0600

    Updates made 10/20/2019 11:20 AM

--- docs/script.js
+++ docs/script.js
@@ -1,2 +1,26 @@
 'use strict mode'
 
+function launchQuiz(){
+
+}
+
+function updateQuestion(){
+
+}
+
+function updateScore(){
+
+}
+
+function renderQuestion(){
+
+}
+
+function displayResults(){
+      
+}
+
+function showHeader()
+   document.getElementById('header-container').html(
+            `<h1>Are you Worthy of the Master Sword?</h1>`
+      )
--- docs/store.js
+++ docs/store.js
@@ -1,59 +1,62 @@
 const STORE = [
+      //Question 1
       {
             question: `Who is the Sage of Shadows in Ocarina of Time?`;
-            choices: [ `Princess Zelda`, `Saria`, `Darunia`, `Impa`];
+            choices: [`Princess Zelda`, `Saria`, `Darunia`, `Impa`];
             answer: `Impa`;
-      };
+      },
+      //Question 2            
       {
-            question:`What is the fairies name that follows the Hero in Majora’s Mask?`;
+            question: `What is the fairies name that follows the Hero in Majora’s Mask?`;
             choices: [`Tatl`, `Termina`, `Twinmold`, `Tael`];
             answer: `Tatl`;
-      };
- What is the first Zelda game that the Hero was an adult the whole game?
-Breath of the Wild
-Ocarina of Time
-Twilight Princess
-Wind Waker
-A: Twilight Princess
- Finish this quote: “Courage need not be remembered, …”
-…for it is never lost.
-…for it is never forgotten.
-…because it is proven.
-…for it is always there.
-A: ...for it is never forgotten.
- The Legend of Zelda (NES) is known for being the first video game that…
-…had music.
-…allowed saving.
-…had color.
-…was released on the NES.
-A: ...allowed saving.
- Who is the main antagonist a majority of The Legend of Zelda series?
-Ganon
-Zant
-Ghirahim
-Majora
-A: Ganon
- Which of The Legend of Zelda games was the first to not have the usual main villain?
-Minish Cap
-Skyward Sword
-The Legend of Zelda II
-Majora’s Mask
-A: The Legend of Zelda II
- What is the Hero’s name?
-Zelda
-Link
-Epona
-Shiek
-A: Link
- How many hearts do you need in Breath of the Wild to get the Master Sword?
-13
-10
-20
-15
-A: 13
- Who develops the Legend of Zelda (series)?
-Sony
-Microsoft
-Sega
-Nintendo
-A: Nintendo
+      },
+      //Question 3            
+      {
+            question: `What is the first Zelda game that the Hero was an adult the whole game?`;
+            choices: [`Breath of the Wild`, `Ocarina of Time`, `Twilight Princess`, `Wind Waker`];
+            answer: `Twilight Princess`;
+            
+      },
+      //Question 4            
+      {
+            question: `Finish this quote: “Courage need not be remembered, …”`;
+            choices:[`...for it is never lost`, `…for it is never forgotten` `…because it is proven` `…for it is always there.`];
+            answer:`...for it is never forgotten.`;
+      },
+      //Question 5      
+      {
+            question: `The Legend of Zelda(NES) is known for being the first video game that…`;
+            choices:[`…had music`,`…allowed saving` `…had color` `…was released on the NES.`];
+            answer: `...allowed saving.`;
+      },
+      //Question 6
+      {
+            question: `Who is the main antagonist a majority of The Legend of Zelda series?`;
+            choices:[`Ganon`, `Zant`, `Ghirahim`, `Majora`];
+            answer: `Ganon`;
+      },
+      //Question 7      
+      {
+            question: `Which of The Legend of Zelda games was the first to not have the usual main villain?`;
+            choices:[`Minish Cap`, `Skyward Sword`, `The Legend of Zelda II`, `Majora’ s Mask`];
+            answer: `The Legend of Zelda II`;
+      },
+      //Question 8
+      {
+            question:`What is the Hero’ s name ?`
+            choices:[`Zelda`, `Link`, `Epona`, `Shiek`];
+            answer:`Link`;
+      },
+      //Question 9
+      {
+            question: `How many hearts do you need in Breath of the Wild to get the Master Sword?`;
+            choices:[`13`, `10`, `20`, `15`];
+            answer: `13`;
+      },
+      //Question 10                  
+      {
+            question: `Who develops the Legend of Zelda(series)?`;
+            choices:[`Sony`, `Microsoft`, `Sega`, `Nintendo`];
+            answer:`Nintendo`;
+      },
